8a203cf36093ac841626b34ddd7bd82897969ae8
Kauã Landi <kaualandi@hotmail.com>
2021-11-24 13:52:35 -0300

    Data.jsx atualizado

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,22 +1,51 @@
 let Data = [
     {
-        id: 0,
         ingles: "I'm sad",
         portugues: "Eu estou triste"
     },
     {
-        id: 1,
         ingles: "Life is better when we smile",
         portugues: "A vida é melhor quando sorrimos"
     },
     {
-        id: 2,
         ingles: "Just do it",
         portugues: "Apenas faça"
     },
     {
-        id: 3,
         ingles: "Temporary people, temporary feelings",
         portugues: "Pessoas temporárias, sentimentos temporários"
-    }
+    },
+    {
+        ingles: "Whatever is good for your soul, do that",
+        portugues: "O que for bom para sua alma, faça isso"
+    },
+    {
+        ingles: "I don’t need your approval to be me",
+        portugues: "Não preciso da sua aprovação para ser eu"
+    },
+    {
+        ingles: "Dream without fear, love without limits",
+        portugues: "Sonhe sem medo, ame sem limites"
+    },
+
+    {
+        ingles: "One bad chapter doesn’t mean your story is over",
+        portugues: "Um capítulo ruim não significa que a sua história acabou"
+    },
+    {
+        ingles: "Enjoy the little things",
+        portugues: "Aproveite as pequenas coisas"
+    },
+    {
+        ingles: "Soul over ego",
+        portugues: "Alma acima do ego"
+    },
+    {
+        ingles: "Live, laugh, love",
+        portugues: "Viva, ria, ame"
+    },
+    {
+        ingles: "A joy shared is a joy doubled",
+        portugues: "Uma alegria compartilhada é uma alegria duplicada"
+    },
 ]; export default Data;
