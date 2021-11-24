8b5fe429e4ecf847e9453a0bdd95eeafda7039d7
Kauã Landi <kaualandi@hotmail.com>
2021-11-24 17:29:38 -0300

    versão 1.0.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { BiHelpCircle } from 'react-icons/bi';
 let Header = () => {
     return (
         <div className="header">
-            <h1>Transcribe The Game</h1>
+            <h1>Transcribe</h1>
             <a href="/help"><BiHelpCircle/></a>
         </div>
     );
